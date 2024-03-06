269aacc77c5f8dba6aa1ab6b4ad427e715d4cbf6
Mohsen <3mohsenfaramarzi@gmail.com>
2024-03-06 22:44:03 +0330

    update loader.js

--- loader.js
+++ loader.js
@@ -3,5 +3,5 @@
 export default function myImageLoader({ src, width, quality }) {
   if (src.startsWith("https://thumbs.wbm.im")) return src;
   if (src.startsWith("https://avatars.githubusercontent.com")) return src;
-  return `https://moneymintt.netlify.app/${src}?w=${width}&q=${quality || 75}`;
+  return `https://moneymint.vercel.app/${src}?w=${width}&q=${quality || 75}`;
 }
